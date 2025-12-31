1dfeac96580b6cc0706e038521d9fc06d4ba4e1d
lukasmueller-crypto <lukasmueller@gmx.net>
2025-12-31 11:30:56 +0100

    Add files via upload

---
+++ src/ParticipantRegister.jsx
@@ -0,0 +1,1 @@
+export default function ParticipantRegister(){return <div>Register</div>;}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+  export const firebaseConfig = {
+  apiKey: "AIzaSyA0HUDogP8bo8uEammGfIaOQgP5dN6JkqQ", // <-- hier DEIN API KEY
+  authDomain: "wichtelorganizer.firebaseapp.com",
+  projectId: "wichtelorganizer",
+  storageBucket: "wichtelorganizer.firebasestorage.app",
+  messagingSenderId: "666069523740",
+  appId: "1:666069523740:web:2466635796efa76d8c6ed9",
+  measurementId: "G-6QBMGNGZJ3",
+};
